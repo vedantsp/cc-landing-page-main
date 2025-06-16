4edeb3340ebbffe906bce69b6477ff477050367e
atharva-git06 <chavanatharva223@gmail.com>
2025-06-16 14:23:31 -0400

    changes for vercel

--- backend/utils/passport.js
+++ backend/utils/passport.js
@@ -18,7 +18,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_SECRET,
-  callbackURL: "/api/auth/google/callback"
+  callbackURL: "https://cc-landing-page-main.vercel.app/api/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     // Check if user already exists
