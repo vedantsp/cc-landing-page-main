1ef6daae771705e3241dc81c85c349b0ea9f80b2
Vedant Pimple <60323634+vedantsp@users.noreply.github.com>
2025-06-20 15:08:41 -0400

    Update App.jsx
    Added Google Analytics tags to all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,59 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation
+} from "react-router-dom";
+
 import { Register } from "./pages/RegisterPage/Register.jsx";
 import { Login } from "./pages/Login";
 import { Home } from "./pages/HomePage/Home";
 import { Logout } from "./pages/Logout";
 import { OAuthSuccess } from "./pages/oAuthSuccess";
-// import ContactUs from "./pages/ContactUs";
 import Partner from "./pages/PartnerPage/Partner.jsx";
 import Invest from "./pages/InvestPage/Invest.jsx";
 import { AboutUs } from "./pages/AboutPage/AboutUs.jsx";
 
+// Helper to fire a GA4 page_view event
+function gtagPageview(path) {
+  if (window.gtag) {
+    window.gtag("event", "page_view", {
+      page_path: path,
+    });
+  }
+}
+
+// Listens for route changes, scrolls to top, and sends page_view
+function ScrollAndTrack() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    gtagPageview(pathname);
+  }, [pathname]);
+
+  return null;
+}
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-      {/* <Navbar />
-      <Header />  change not up to date
-       */ }
-        <Routes>
+    <BrowserRouter>
+      {/* this component runs on every route change */}
+      <ScrollAndTrack />
+
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/oauth-success" element={<OAuthSuccess />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          < Route path = "/logout" element = {<Logout />}/>
-          < Route path = "/aboutUs" element = {<AboutUs />}/>
-          {/* < Route path = "/contactUs" element = {<ContactUs />}/> */}
-          < Route path = "/invest" element = {<Invest/>}/>
-          < Route path = "/partner" element = {<Partner />}/>
-
-
-        </Routes>
-        {/* <Footer /> */}
-      </BrowserRouter>
-    </>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/invest" element={<Invest />} />
+        <Route path="/partner" element={<Partner />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
+
 export default App;
